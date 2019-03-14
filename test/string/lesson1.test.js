3987a2a3526b5d0633cbb353bdc669ba5880b2f4
kongkaiyu <kaiyu@eastmoney.com>
2019-03-14 13:55:23 +0800

    电话组合

--- test/string/lesson1.test.js
+++ test/string/lesson1.test.js
@@ -1,5 +1,5 @@
-import splitWord from '../../code/string/lesson1'
+import revertByWorld from '../../code/string/lesson1'
 
-test("splitWord(let's us go to Beijing)", () => {
-  expect(splitWord("let's us go to Beijing")).toBe("s'tel su og ot gnijieB")
+test("revertByWorld:let's us go to Beijing", () => {
+  expect(revertByWorld("let's us go to Beijing")).toBe("s'tel su og ot gnijieB")
 })
